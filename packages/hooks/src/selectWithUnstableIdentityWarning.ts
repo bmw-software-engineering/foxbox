265732c4bcc7310fabaedd237df4533da7d2014f
Franck Labracherie <franck.labracherie@partner.bmw.de>
2024-07-05 10:32:34 +0200

    Fixed existing tests

--- packages/hooks/src/selectWithUnstableIdentityWarning.ts
+++ packages/hooks/src/selectWithUnstableIdentityWarning.ts
@@ -1,6 +1,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
+import * as _ from "lodash-es";
+
 import Log from "@foxglove/log";
 
 const log = Log.getLogger(__filename);
@@ -11,7 +13,7 @@ export function selectWithUnstableIdentityWarning<T, U>(value: T, selector: (val
     const secondResult = selector(value);
     if (result !== secondResult) {
       log.warn(`Selector ${selector.toString()} produced different values for the same input.
-  This will cause unecesessery re-renders of your component.`);
+      This will cause unecesessery re-renders of your component.`);
     }
     return secondResult;
   }
